da190aa28f91bb033d3c8cd6186a23729ef3cdde
burtonjong <burtonjong05@gmail.com>
2025-01-28 13:17:49 -0700

    add backend

---
+++ backend/src/main/java/com/seng401/endless_odyssey/EndlessOdysseyApplication.java
@@ -0,0 +1,13 @@
+package com.seng401.endless_odyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndlessOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndlessOdysseyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seng401/endless_odyssey/EndlessOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seng401.endless_odyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndlessOdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
