07117023abde15334a175850eb4652420502c98b
burtonjong <burtonjong05@gmail.com>
2025-01-30 09:45:16 -0700

    delete template code

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -1,23 +1,8 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
-
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <div className="flex gap-8 justify-center items-center"></div>
+      <h1 className="sr-only">Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
         The fastest way to build apps with{" "}
         <a
@@ -26,7 +11,7 @@ export default function Header() {
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Supabase
+          sasd
         </a>{" "}
         and{" "}
         <a
@@ -35,7 +20,7 @@ export default function Header() {
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Next.js
+          sss
         </a>
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
