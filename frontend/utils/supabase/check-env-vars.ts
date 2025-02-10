06d27f08e76fc8136e7b87b65f62fa688b4a21c9
burtonjong <burtonjong05@gmail.com>
2025-02-09 21:18:37 -0700

    change dockerfile

--- frontend/utils/supabase/check-env-vars.ts
+++ frontend/utils/supabase/check-env-vars.ts
@@ -1,6 +1,8 @@
 // This check can be removed
 // it is just for tutorial purposes
 
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 export const hasEnvVars =
   process.env.NEXT_PUBLIC_SUPABASE_URL &&
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
