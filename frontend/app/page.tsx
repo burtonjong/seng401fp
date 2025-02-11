81a1227ce7023a468cf16780eac3a9bc3bd13257
burtonjong <burtonjong05@gmail.com>
2025-02-11 08:50:11 -0700

    replace placeholder names and titles

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,9 @@ export default async function Home() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">s</h2>
+        <h2 className="font-medium text-xl mb-4 text-center">
+          A never ending...
+        </h2>
       </main>
     </>
   );
--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -2,27 +2,11 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center"></div>
-      <h1 className="sr-only">Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          test
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          sss
-        </a>
-      </p>
+      <h1 className="sr-only">Endless Odyssey</h1>
+      <h2 className="font-bold text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+        {" "}
+        Endless Odyssey
+      </h2>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
