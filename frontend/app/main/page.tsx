981a17fbdf95bf80e42e5b1e1806ed455a08891a
promatty <matthewlblue@gmail.com>
2025-03-10 13:32:27 -0600

    update css files w/ color palette

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -155,10 +155,10 @@ export default function Main() {
 				{/* Chat area */}
 				<div className="flex-1 flex flex-col items-center justify-center p-4 overflow-auto">
 					<h1 className="text-4xl font-medium">
-						<span className="bg-gradient-to-r from-[#52B69A] to-[#34A0A4] bg-clip-text text-transparent">
+						<span className="bg-gradient-to-r from-[var(--gradient-5)] to-[var(--gradient-6)] bg-clip-text text-transparent">
 							Hello,
 						</span>
-						<span className="bg-gradient-to-r from-[#34A0A4] to-[#168AAD] bg-clip-text text-transparent">
+						<span className="bg-gradient-to-r from-[var(--gradient-6)] to-[var(--gradient-7)] bg-clip-text text-transparent">
 							{" "}
 							(Insert Name Here)
 						</span>
