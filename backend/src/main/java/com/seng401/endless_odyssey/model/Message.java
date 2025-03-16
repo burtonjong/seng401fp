41a38fabfcb13d912034d2188adb28837078b7eb
promatty <matthewlblue@gmail.com>
2025-03-16 13:50:59 -0600

    fix column error

--- backend/src/main/java/com/seng401/endless_odyssey/model/Message.java
+++ backend/src/main/java/com/seng401/endless_odyssey/model/Message.java
@@ -15,8 +15,7 @@ public class Message {
     private LocalDateTime createdAt;
 
     @ManyToOne
-    @JoinColumn(name = "story_id", referencedColumnName = "id")
-    @Column(nullable = false)
+    @JoinColumn(name = "story_id", referencedColumnName = "id", nullable = false)
     private Story story;
 
     @Column(nullable = false)
