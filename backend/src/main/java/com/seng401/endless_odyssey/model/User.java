2570e0d2b7b3a61ce97df1bdbba661df4b0d2202
Matthew McDougall <burke.mcdougall@ucalgary.ca>
2025-03-16 10:03:48 -0600

    setup for model class user

--- backend/src/main/java/com/seng401/endless_odyssey/model/User.java
+++ backend/src/main/java/com/seng401/endless_odyssey/model/User.java
@@ -1,15 +1,49 @@
 package com.seng401.endless_odyssey.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
 
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 3, max = 50)
+    @Column(nullable = false)
     private String name;
+
+    @NotNull
+    @Size(min = 8)
+    @Column(nullable = false)
+    private String password;
+
+    @NotNull
+    @Email
+    @Size(min = 5, max = 100)
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public User() {}
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -19,4 +53,28 @@ public class User {
         this.name = name;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
