066af48fdd9ed3b81305ff207f60ed2bade59bf8
Matthew McDougall <burke.mcdougall@ucalgary.ca>
2025-03-19 13:54:48 -0600

    update direct db fetch to route through backend

--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -1,8 +1,17 @@
-import { createClient } from "@/utils/supabase/server";
-
 export default async function Users() {
-  const supabase = await createClient();
-  const { data: user } = await supabase.from("user").select();
+  const response = await fetch("http://localhost:8080/api/stories/user", {
+    method: "GET",
+  });
 
-  return <pre>{JSON.stringify(user, null, 2)}</pre>;
+  if (!response.ok) {
+    return <p>Error occured fetching user data</p>;
+  }
+
+  const user = await response.json();
+  return (
+    <div>
+      <h2>User Data:</h2>
+      <pre>{JSON.stringify(user, null, 2)}</pre>  {}
+    </div>
+  );
 }
